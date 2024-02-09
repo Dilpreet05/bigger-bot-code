a52afac229a696c8cc338dd601d2bd261ae218f2
Dilpreet <67494637+Dilpreet05@users.noreply.github.com>
2024-02-09 18:19:24 -0500

    a

--- src/main.cpp
+++ src/main.cpp
@@ -75,27 +75,27 @@ void opcontrol() {
     
     // PID Tuner
     // After you find values that you're happy with, you'll have to set them in auton.cpp
-    if (!pros::competition::is_connected()) { 
-      // Enable / Disable PID Tuner
-      //  When enabled: 
-      //  * use A and Y to increment / decrement the constants
-      //  * use the arrow keys to navigate the constants
-      if (master.get_digital_new_press(DIGITAL_X)) 
-        chassis.pid_tuner_toggle();
+    // if (!pros::competition::is_connected()) { 
+    //   // Enable / Disable PID Tuner
+    //   //  When enabled: 
+    //   //  * use A and Y to increment / decrement the constants
+    //   //  * use the arrow keys to navigate the constants
+    //   if (master.get_digital_new_press(DIGITAL_X)) 
+    //     chassis.pid_tuner_toggle();
         
-      // Trigger the selected autonomous routine
-      if (master.get_digital_new_press(DIGITAL_B)) 
-        autonomous();
+    //   // Trigger the selected autonomous routine
+    //   if (master.get_digital_new_press(DIGITAL_B)) 
+    //     autonomous();
 
-      chassis.pid_tuner_iterate(); // Allow PID Tuner to iterate
-    } 
+    //   chassis.pid_tuner_iterate(); // Allow PID Tuner to iterate
+    // } 
 
 
 
     updateDrive();
-    // spinFlywheel();
-    // spinIntake();
-    // updatePistons();
+    spinFlywheel();
+    spinIntake();
+    updatePistons();
 
     pros::delay(ez::util::DELAY_TIME); // This is used for timer calculations!  Keep this ez::util::DELAY_TIME
   }
