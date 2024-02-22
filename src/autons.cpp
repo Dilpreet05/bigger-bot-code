9bb74d1a8f4dc38c3391e0e4fd66a1f574faf4cb
Dilpreet <67494637+Dilpreet05@users.noreply.github.com>
2024-02-22 12:21:09 -0500

    a

--- src/autons.cpp
+++ src/autons.cpp
@@ -85,7 +85,7 @@ void scoreAllianceTriball(){
   grabberDown();
   pros::delay(500);
 
-  chassis.pid_drive_set(-62,40,true);
+  chassis.pid_drive_set(-62,80,true);
   chassis.pid_wait();
 
   // chassis.pid_swing_set(ez::RIGHT_SWING, 0, TURN_SPEED);
